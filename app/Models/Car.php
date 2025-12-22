f038bb30744000b6538d114ebbcb47b7d92b3325
Muad-Ahmed <muad8010@gmail.com>
2025-12-22 10:57:37 +0300

    feat: define core relationships for Car, CarImage, and CarType models

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Car extends Model
@@ -26,4 +29,25 @@ class Car extends Model
         'description',
         'published_at',
     ];
+
+    public function features(): HasOne
+    {
+        return $this->hasOne(CarFeatures::class, 'car_id');
+    }
+
+    public function primaryImage(): HasOne
+    {
+        return $this->hasOne(CarImage::class)->oldestOfMany('position');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(CarImage::class, 'car_id'); // car_id is optional
+    }
+
+    public function carType(): BelongsTo
+    {
+        return $this->belongsTo(CarType::class, 'car_type_id'); // car_type_id is optional
+    }
+    
 }
