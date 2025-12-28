b189af1040db764912831f3f2a4fa4e885d0208a
Muad-Ahmed <muad8010@gmail.com>
2025-12-28 10:34:05 +0300

    feat: persist selected values in search components
    Added @selected directive to various select components using attribute value binding to ensure search filters remain active after form submission.

--- resources/views/components/select-model.blade.php
+++ resources/views/components/select-model.blade.php
@@ -1,7 +1,7 @@
 <select id="modelSelect" name="model_id">
     <option value="" style="display: block">Model</option>
     @foreach ($models as $model)
-        <option value="{{ $model->id }}" data-parent="{{ $model->maker_id }}">
+        <option value="{{ $model->id }}" @selected($attributes->get('value') == $model->id) data-parent="{{ $model->maker_id }}">
             {{ $model->name }}
         </option>
     @endforeach
