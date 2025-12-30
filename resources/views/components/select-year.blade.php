003843efced6f54f89a26997f3365de3f779f82f
Muad-Ahmed <muad8010@gmail.com>
2025-12-30 10:56:01 +0300

    feat: implement value persistence for radio and year components

--- resources/views/components/select-year.blade.php
+++ resources/views/components/select-year.blade.php
@@ -1,10 +1,10 @@
 @php
-    $year = date('Y')
+    $year = date('Y');
 @endphp
 
 <select name="year">
     <option value="">Year</option>
     @for ($i = $year; $i >= 1970; $i--)
-        <option value="{{ $i }}">{{ $i }}</option>
+        <option value="{{ $i }}" @selected($attributes->get('value') == $i)>{{ $i }}</option>
     @endfor
 </select>
