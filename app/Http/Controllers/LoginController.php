cfabcd489d15b7378e99710495ea8a5720231821
Muad-Ahmed <muad8010@gmail.com>
2026-01-01 09:39:57 +0300

    feat: implement login store logic with validation and session regeneration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -10,4 +11,24 @@ class LoginController extends Controller
     {
         return view('auth.login');
     }
+
+    public function store(Request $request)
+{
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required', 'string']
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        // If that was successful, regenerate session
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('home'))
+            ->with('success', 'Welcome Back');
+    }
+
+    return  redirect()->back()->withErrors([
+        'email' => 'The provided credentials do not match our records'
+    ])->onlyInput('email');
+}
 }
