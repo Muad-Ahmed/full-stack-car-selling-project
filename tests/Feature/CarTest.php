740eb2bf50936c77febe1c44739d46a736f2dbf9
Muad-Ahmed <muad8010@gmail.com>
2026-01-11 14:04:59 +0300

    test: add access control tests for car index page

--- tests/Feature/CarTest.php
+++ tests/Feature/CarTest.php
@@ -6,8 +6,8 @@ use function Pest\Laravel\get;
 it('should not be possible to access car create page as guest user', function () {
 
     get(route('car.create'))
-    ->assertRedirectToRoute('login')
-    ->assertStatus(302);
+        ->assertRedirectToRoute('login')
+        ->assertStatus(302);
 });
 
 it('should be possible to access car create page as authenticated user', function () {
@@ -17,4 +17,17 @@ it('should be possible to access car create page as authenticated user', functio
         ->get(route('car.create'))
         ->assertOk()
         ->assertSee("Add new car");
+});
+
+it('should not be possible to access my cars page as guest user', function () {
+    get(route('car.index'))
+        ->assertRedirectToRoute('login');
+});
+
+it('should be possible to access my cars page as authenticated user', function () {
+    $user = \App\Models\User::factory()->create();
+    actingAs($user)
+        ->get(route('car.index'))
+        ->assertOk()
+        ->assertSee("My Cars");
 });
