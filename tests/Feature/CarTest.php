be8856627d4a048c6ed110d626b091d8de448112
Muad-Ahmed <muad8010@gmail.com>
2026-01-11 14:02:14 +0300

    test: implement access control tests for car creation

---
+++ tests/Feature/CarTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+it('should not be possible to access car create page as guest user', function () {
+
+    get(route('car.create'))
+    ->assertRedirectToRoute('login')
+    ->assertStatus(302);
+});
+
+it('should be possible to access car create page as authenticated user', function () {
+    $user = \App\Models\User::factory()->create();
+
+    actingAs($user)
+        ->get(route('car.create'))
+        ->assertOk()
+        ->assertSee("Add new car");
+});
