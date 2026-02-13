24d12e63f851230924d2d4c6921f507a09201445
Muad-Ahmed <muad8010@gmail.com>
2026-02-13 06:16:51 +0300

    feat: enhance my cars table responsive design

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -1,86 +1,91 @@
 <x-app-layout bodyClass="page-my-cars">
     <main>
-        <div>
+        <div class="section-padding">
             <div class="container">
-                <h1 class="car-details-page-title">My Cars</h1>
-                <div class="card p-medium">
+                <div class="page-header">
+                    <h1 class="car-details-page-title">My Cars</h1>
+                    <a href="{{ route('car.create') }}" class="btn btn-primary">Add New Car</a>
+                </div>
+
+                <div class="card">
                     <div class="table-responsive">
-                        <table class="table">
+                        <table class="table my-cars-table">
                             <thead>
                                 <tr>
                                     <th>Image</th>
                                     <th>Title</th>
-                                    <th>Date</th>
-                                    <th>Published</th>
-                                    <th>Actions</th>
+                                    <th class="hide-on-mobile">Date</th>
+                                    <th class="hide-on-mobile">Published</th>
+                                    <th class="text-center">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @forelse($cars as $car)
                                     <tr>
-                                        <td>
+                                        <td data-label="Image">
                                             <img src="{{ $car->primaryImage?->getUrl() ?: '/img/no_image.jpg' }}"
                                                 alt="" class="my-cars-img-thumbnail" />
                                         </td>
-                                        <td>
+                                        <td data-label="Title" class="font-bold">
                                             {{ $car->getTitle() }}
+
                                         </td>
-                                        <td>{{ $car->getCreateDate() }}</td>
-                                        <td>{{ $car->published_at ? 'Yes' : 'No' }}</td>
-                                        <td class="">
-                                            <a href="{{ route('car.edit', $car) }}"
-                                                class="btn btn-edit inline-flex items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    style="width: 12px; margin-right: 5px">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                                </svg>
-
-                                                edit
-                                            </a>
-                                            <a href="{{ route('car.images', $car) }}"
-                                                class="btn btn-edit inline-flex items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    style="width: 12px; margin-right: 5px">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                                </svg>
-                                                images
-                                            </a>
-                                            <form action="{{ route('car.destroy', $car) }}" method="POST"
-                                                class="inline-flex">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button
-                                                    onclick="return confirm('Are you sure you want to delete this car?')"
-                                                    class="btn btn-delete inline-flex items-center">
+                                        <td data-label="Date" class="hide-on-mobile">{{ $car->getCreateDate() }}</td>
+                                        <td data-label="Published" class="hide-on-mobile">
+                                            <span
+                                                class="badge {{ $car->published_at ? 'badge-success' : 'badge-warning' }}">
+                                                {{ $car->published_at ? 'Yes' : 'No' }}
+                                            </span>
+                                        </td>
+                                        <td data-label="Actions" class="actions-cell">
+                                            <div class="actions-wrapper">
+                                                <a href="{{ route('car.edit', $car) }}" class="btn-action btn-edit-icon"
+                                                    title="Edit">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        style="width: 12px; margin-right: 5px">
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                                     </svg>
-
-                                                    delete
-                                                </button>
-                                            </form>
+                                                </a>
+                                                <a href="{{ route('car.images', $car) }}"
+                                                    class="btn-action btn-images-icon" title="Images">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                                    </svg>
+                                                </a>
+                                                <form action="{{ route('car.destroy', $car) }}" method="POST"
+                                                    class="inline-form">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" onclick="return confirm('Are you sure?')"
+                                                        class="btn-action btn-delete-icon" title="Delete">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5"
+                                                            stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td colspan="5" class="text-center p-large">
-                                            You don't have any cars yet. <a href="{{ route('car.create') }}">Add new
-                                                car</a>
+                                        <td colspan="5" class="text-center empty-state">
+                                            You don't have any cars yet. <a href="{{ route('car.create') }}"
+                                                class="link-primary">Add new car</a>
                                         </td>
                                     </tr>
                                 @endforelse
                             </tbody>
                         </table>
                     </div>
-
-                    {{ $cars->onEachSide(1)->links() }}
+                    <div class="pagination-wrapper">
+                        {{ $cars->onEachSide(1)->links() }}
+                    </div>
                 </div>
             </div>
         </div>
